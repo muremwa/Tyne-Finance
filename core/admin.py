c4d0e11b71482fa40bad5cd5b274bdc4fd79ac45
Daniel Muremwa Mburu <muremwa@users.noreply.github.com>
2023-05-22 18:27:14 +0300

    modified admin

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 from .models import Currency, User
 
-admin.site.register(Currency)
+
+@admin.register(Currency)
+class CurrencyModelAdmin(admin.ModelAdmin):
+    list_display = ('country', 'code', 'symbol')
+    search_fields = ('country', 'code')
+    actions = None
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(User)
