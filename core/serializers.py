b3bc6be5c058a916aee4368799fc8ee831869289
Daniel Muremwa Mburu <muremwa@users.noreply.github.com>
2023-05-30 19:30:09 +0300

    adds user serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,9 @@
-from django.core.exceptions import PermissionDenied
+from typing import Dict
+
+from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
 from django.utils.translation import gettext_lazy as _
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, IntegerField, ValidationError
+from rest_framework.fields import empty
 
 from .models import User, Currency, Account, AccountType
 
@@ -26,3 +29,53 @@ class AccountTypeSerializer(NoEditOrCreateModelSerializer):
     class Meta:
         model = AccountType
         fields = ('name', 'code')
+
+
+class UserSerializer(ModelSerializer):
+    user_currency = CurrencySerializer(source='currency', required=False)
+    currency = IntegerField(required=True, write_only=True)
+    __cache = {}
+
+    def __init__(self, instance=None, data=empty, **kwargs):
+        super().__init__(instance, data, **kwargs)
+
+        if instance:
+            for field in self.fields.values():
+                if hasattr(field, 'required'):
+                    field.required = False
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'is_active', 'currency', 'password', 'user_currency')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_active': {'read_only': True}
+        }
+
+    def validate_currency(self, value):
+        try:
+            self.__cache.update({
+                'currency': Currency.objects.get(pk=value)
+            })
+        except ObjectDoesNotExist:
+            raise ValidationError(_(f'No currency with id "{value}"'))
+
+    def update_validated_data_with_currency(self, validated_data: Dict):
+        if currency := self.__cache.get('currency'):
+            validated_data.update({'currency': currency})
+        else:
+            raise ObjectDoesNotExist(_('No currency found'))
+
+        return validated_data
+
+    def create(self, validated_data: Dict) -> User:
+        return super().create(
+            self.update_validated_data_with_currency(validated_data)
+        )
+
+    def update(self, instance, validated_data: Dict):
+        if 'currency' in validated_data.keys():
+            validated_data = self.update_validated_data_with_currency(
+                validated_data
+            )
+        return super().update(instance, validated_data)
--- core/tests/test_serializer.py
+++ core/tests/test_serializer.py
@@ -2,13 +2,14 @@ from django.test import TestCase
 from django.core.exceptions import PermissionDenied
 
 from core.models import Currency, User, Account, AccountType
-from core.serializers import CurrencySerializer, NoEditOrCreateModelSerializer, AccountTypeSerializer
+from core.serializers import CurrencySerializer, NoEditOrCreateModelSerializer, AccountTypeSerializer, UserSerializer
 
 
 class CoreSerializerTestCase(TestCase):
 
     def setUp(self) -> None:
         self.currency = Currency.objects.create(country='Kenya', code='KES', symbol='Ksh')
+        self.currency_2 = Currency.objects.create(country='Tanzania', code='TZS', symbol='Tsh')
         self.user = User.objects.create(
             username='tyne',
             email='tyne@tfinance.io',
@@ -48,3 +49,64 @@ class CoreSerializerTestCase(TestCase):
             act.data,
             {'name': self.account_type.name, 'code': self.account_type.code}
         )
+
+    def test_user_serializer(self):
+        us = UserSerializer(self.user)
+        self.assertDictEqual(
+            us.data,
+            {
+                'username': self.user.username,
+                'first_name': self.user.first_name,
+                'last_name': self.user.last_name,
+                'email': self.user.email,
+                'is_active': self.user.is_active,
+                'user_currency': {
+                    'country': self.currency.country,
+                    'code': self.currency.code
+                }
+            }
+        )
+
+    def test_user_serializer_create(self):
+        new_user = UserSerializer(data={
+            'username': 'big',
+            'email': 'big@tyne.com',
+            'password': '12345',
+            'currency': 1900
+        })
+        self.assertFalse(new_user.is_valid())
+        new_user = UserSerializer(data={
+            'username': 'big',
+            'email': 'big@tyne.com',
+            'password': '12345',
+            'currency': self.currency.pk
+        })
+        self.assertTrue(new_user.is_valid())
+        big_user = new_user.save()
+        big_user_q: User = User.objects.get(username__iexact='big')
+        self.assertIsNotNone(big_user)
+        self.assertIsNotNone(big_user_q)
+        self.assertEquals(big_user_q.pk, big_user.pk)
+        self.assertEquals(big_user_q.currency.pk, big_user.currency.pk)
+
+    def test_user_serializer_edit(self):
+        ed_user = UserSerializer(data={
+            'currency': 2000
+        }, instance=self.user)
+        self.assertFalse(ed_user.is_valid())
+        ed_user = UserSerializer(data={
+            'currency': self.currency_2.pk
+        }, instance=self.user)
+        self.assertTrue(ed_user.is_valid())
+        self.assertEquals(self.user.currency.pk, self.currency.pk)
+        bg_user = ed_user.save()
+        self.user.refresh_from_db()
+        self.assertEquals(bg_user.pk, self.user.pk)
+        self.assertEquals(self.user.currency.pk, self.currency_2.pk)
+        ed_user = UserSerializer(data={
+            'username': 'jim'
+        }, instance=self.user)
+        self.assertTrue(ed_user.is_valid())
+        ed_user.save()
+        self.user.refresh_from_db()
+        self.assertEquals(self.user.username, 'jim')
